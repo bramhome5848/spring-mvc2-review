aa1332f5af9d1339bf9738ca71917d66d6efddfb
KyungjinLee <bramhome5848@gmail.com>
2023-09-16 15:26:46 +0900

    20230916 섹션6 로그인 처리1 - 쿠키,세션 - 2. 로그인 처리하기 - 쿠키

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,7 +1,12 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -15,10 +20,33 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //@GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    /**
+     * required -> false
+     * 로그인 하지 않은 사용자(쿠키 값이 없는 사용자)도 들어오기 때문에 false 로 지정
+     */
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if(memberId == null) {
+            return "home";
+        }
+
+        Member loginMember = memberRepository.findById(memberId);   //로그인
+
+        if(loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -23,8 +26,14 @@ public class LoginController {
         return "/login/loginForm";
     }
 
-    @PostMapping("login")
-    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    /**
+     * 쿠키
+     - 영속 쿠키: 만료 날짜를 입력하면 해당 날짜까지 유지
+     - 세션 쿠키: 만료 날짜를 생략하면 브라우저 종료시 까지만 유지
+     */
+    @PostMapping("/login")
+    public String login(@Validated @ModelAttribute LoginForm form, BindingResult bindingResult,
+                        HttpServletResponse response) {
         if(bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -37,6 +46,21 @@ public class LoginController {
             return "login/loginForm";
         }
 
+        //쿠키에 시간 정보를 주지 않으면 세션 쿠키(브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    private String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
