9e1b23277d5d5ed02d986f13931e6cc082436f71
KyungjinLee <bramhome5848@gmail.com>
2023-09-21 17:56:40 +0900

    20230921 섹션7 로그인 처리2 - 필터,인터셉터 - 2. 스프링 인터셉터

--- login/src/main/java/hello/login/web/WebConfig.java
+++ login/src/main/java/hello/login/web/WebConfig.java
@@ -2,9 +2,12 @@ package hello.login.web;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
@@ -12,19 +15,31 @@ import javax.servlet.Filter;
  * 필터를 등록하는 방법은 여러가지가 있지만, 스프링 부트를 사용한다면 FilterRegistrationBean 을 사용해서 등록
  */
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())   //등록할 인터셉터 지정
+                .order(1)                               //순서
+                .addPathPatterns("/**")                 //인터셉터를 적용할 URL 패턴
+                .excludePathPatterns("/css/**", "/*.ico", "/error");    //인터셉터에서 제외할 패턴을 지정
 
-    @Bean
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
+    }
+
+    //@Bean
     public FilterRegistrationBean<Filter> logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());  //등록할 필터를 지정
-        filterRegistrationBean.setOrder(1); //필터는 체인으로 동작 -> 따라서 순서가 필요(낮을 수록 먼저 동작)
-        filterRegistrationBean.addUrlPatterns("/*");    //필터를 적용할 URL 패턴을 지정
+        filterRegistrationBean.setOrder(1);                 //필터는 체인으로 동작 -> 따라서 순서가 필요(낮을 수록 먼저 동작)
+        filterRegistrationBean.addUrlPatterns("/*");        //필터를 적용할 URL 패턴을 지정
 
         return filterRegistrationBean;
     }
 
-    @Bean
+    //@Bean
     public FilterRegistrationBean<Filter> loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
